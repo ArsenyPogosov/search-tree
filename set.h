eef09982afd7a98874b2daa651445b5e41038653
Pogosov Arseny <pogosovarseny@gmail.com>
2022-03-27 22:16:00 +0300

    Some problems resolved.

--- set.h
+++ set.h
@@ -7,22 +7,19 @@ public:
     Set() { _sz = 0; _root = nullptr; }
 
     template<typename Iterator>
-    Set(Iterator first, Iterator last)
-    {
+    Set(Iterator first, Iterator last) {
         _sz = 0; _root = nullptr;
         for (; first != last; ++first)
             insert(*first);
     }
 
-    Set(std::initializer_list<T> elems)
-    {
+    Set(std::initializer_list<T> elems) {
         _sz = 0; _root = nullptr;
         for (auto i : elems)
             insert(i);
     }
 
-    Set& operator=(const Set& s)
-    {
+    Set& operator=(const Set& s) {
         if (this == &s)
             return *this;
 
@@ -30,8 +27,7 @@ public:
         if (_root)
             delete _root;
         _root = nullptr;
-        if (s._root)
-        {
+        if (s._root) {
             _root = new Node();
             *_root = *s._root;
         }
@@ -39,15 +35,13 @@ public:
         return *this;
     }
 
-    Set(const Set& s)
-    {
+    Set(const Set& s) {
         _sz = 0;
         _root = nullptr;
         *this = s;
     }
 
-    ~Set()
-    {
+    ~Set() {
         if (_root)
             delete _root;
     }
@@ -61,14 +55,16 @@ public:
     }
 
     void insert(const T& elem) {
-        if (find(elem) == end())
+        if (find(elem) == end()) {
             ++_sz;
+        }
         _root = insert(_root, elem);
     }
 
     void erase(const T& elem) {
-        if (find(elem) != end())
+        if (find(elem) != end()) {
             --_sz;
+        }
         _root = erase(_root, elem);
     }
 
@@ -76,144 +72,140 @@ private:
     struct Node
     {
         T data;
-        Node* l, * r, * p;
+        Node *left, *right, *parrent;
         size_t h;
 
-        Node() { l = r = p = nullptr; h = 1; };
-        Node(T data) : data(data) { l = r = p = nullptr; h = 1; };
-        Node& operator=(const Node& node)
-        {
+        Node() { 
+            left = right = parrent = nullptr; 
+            h = 1; 
+        };
+        Node(T data) : data(data) { 
+            left = right = parrent = nullptr; 
+            h = 1; 
+        };
+        Node& operator=(const Node& node) {
             data = node.data;
             h = node.h;
-            /*if (l)
-                delete l;*/
-            l = nullptr;
-            /*if (r)
-                delete r;*/
-            r = nullptr;
-            if (node.l)
-            {
-                l = new Node();
-                *l = *node.l;
-                l->p = this;
+            left = nullptr;
+            right = nullptr;
+            if (node.left) {
+                left = new Node();
+                *left = *node.left;
+                left->parrentarrent = this;
             }
-            if (node.r)
-            {
-                r = new Node();
-                *r = *node.r;
-                r->p = this;
+            if (node.right) {
+                right = new Node();
+                *right = *node.right;
+                right->parrentarrent = this;
             }
 
             return *this;
         }
 
-        ~Node()
-        {
-            if (this->l)
-                delete this->l;
-            if (this->r)
-                delete this->r;
+        ~Node() {
+            if (this->left) {
+                delete this->left;
+            }
+            if (this->right) {
+                delete this->right;
+            }
         }
     };
 
 public:
-    class iterator
-    {
+    class iterator {
     public:
         friend class Set;
 
-        iterator() { _current = nullptr; }
+        iterator() { 
+            _current = nullptr; 
+        }
 
-        iterator(const iterator& it) 
-        {
+        iterator(const iterator& it) {
             _current = it._current;
         }
 
-        ~iterator() {}
+        ~iterator() {
+        }
 
-        iterator& operator=(const iterator& it) 
-        {
+        iterator& operator=(const iterator& it) {
             _current = it._current;
 
             return *this;
         }
 
-        iterator& operator++() 
-        {
+        iterator& operator++() {
             Node* res = _current;
-            if (res->r)
-            {
-                res = res->r;
-                while (res->l)
-                    res = res->l;
+            if (res->right) {
+                res = res->right;
+                while (res->left)
+                    res = res->left;
 
                 return *this = iterator(res, _root);
             }
-            while (true)
-            {
-                Node* parent = res->p;
-                if (!parent)
+            while (true) {
+                Node* parent = res->parrent;
+                if (!parent) {
                     return *this = iterator(nullptr, _root);
+                }
 
-                if (parent->l == res)
+                if (parent->left == res) {
                     return *this = iterator(parent, _root);
+                }
 
                 res = parent;
             }
         }
 
-        iterator operator++(int)
-        {
+        iterator operator++(int) {
             iterator res = *this;
-            ++*this;
+            ++* this;
 
             return res;
         }
 
-        iterator& operator--() 
-        {
-            if (!_current)
-            {
+        iterator& operator--() {
+            if (!_current) {
                 Node* res = _root;
-                while (res->r)
-                    res = res->r;
+                while (res->right)
+                    res = res->right;
 
                 return *this = iterator(res, _root);
             }
 
             Node* res = _current;
-            if (res->l)
-            {
-                res = res->l;
-                while (res->r)
-                    res = res->r;
+            if (res->left) {
+                res = res->left;
+                while (res->right)
+                    res = res->right;
 
                 return *this = iterator(res, _root);
             }
-            while (true)
-            {
-                Node* parent = res->p;
-                if (!parent)
+            while (true) {
+                Node* parent = res->parrent;
+                if (!parent) {
                     return *this = iterator(nullptr, _root);
+                }
 
-                if (parent->r == res)
+                if (parent->right == res) {
                     return *this = iterator(parent, _root);
+                }
 
                 res = parent;
             }
         }
 
-        iterator operator--(int)
-        {
+        iterator operator--(int) {
             iterator res = *this;
-            --*this;
+            --* this;
 
             return res;
         }
 
         const T& operator*() const {
-            if (!_current)
+            if (!_current) {
                 return _root->data;
+            }
             return _current->data;
         }
 
@@ -230,16 +222,18 @@ public:
         }
 
     private:
-        iterator(Node* current, Node* root) : _current(current), _root(root) {}
-        Node* _current,* _root;
+        iterator(Node* current, Node* root) : _current(current), _root(root) {
+        }
+        Node* _current, * _root;
     };
 
     iterator begin() const {
         if (!_root) return end();
 
         Node* res = _root;
-        while (res->l)
-            res = res->l;
+        while (res->left) {
+            res = res->left;
+        }
         return iterator(res, _root);
     }
 
@@ -249,179 +243,159 @@ public:
 
     iterator find(const T& elem) const {
         iterator res = lower_bound(elem);
-        if (res != end() && (!(*res < elem) && !(elem < *res)))
+        if (res != end() && (!(*res < elem) && !(elem < *res))) {
             return res;
+        }
 
         return end();
     }
 
     iterator lower_bound(const T& elem) const {
         Node* res = _root;
-        while (res)
-        {
-            if (res->data < elem)
-            {
-                if (!res->r)
+        while (res) {
+            if (res->data < elem) {
+                if (!res->right) {
                     break;
-                else
-                    res = res->r;
-            }
-            else
-            {
-                if (!res->l)
+                } else {
+                    res = res->right;
+                }
+            } else {
+                if (!res->left) {
                     break;
-                else
-                    res = res->l;
+                } else {
+                    res = res->left;
+                }
             }
         }
 
-        while (res && res->data < elem)
-            res = res->p;
+        while (res && res->data < elem) {
+            res = res->parrent;
+        }
 
         return iterator(res, _root);
     }
 
 private:
     Node* LRotate(Node* t) {
-        Node* r = t->r;
-        Node* L = r->l;
-        r->p = t->p;
-        r->l = t; if (t) t->p = r;
-        t->r = L; if (L) L->p = t;
-        t->h = std::max(Height(t->l), Height(t->r)) + 1;
-        r->h = std::max(Height(r->l), Height(r->r)) + 1;
+        Node* r = t->right;
+        Node* L = r->left;
+        r->parrent = t->parrent;
+        r->left = t; if (t) t->parrent = r;
+        t->right = L; if (L) L->parrent = t;
+        t->h = std::max(Height(t->left), Height(t->right)) + 1;
+        r->h = std::max(Height(r->left), Height(r->right)) + 1;
 
         return r;
     }
 
     Node* RRotate(Node* t) {
-        Node* l = t->l;
-        Node* R = l->r;
-        l->p = t->p;
-        l->r = t; if (t) t->p = l;
-        t->l = R; if (R) R->p = t;
-        t->h = std::max(Height(t->l), Height(t->r)) + 1;
-        l->h = std::max(Height(l->l), Height(l->r)) + 1;
+        Node* l = t->left;
+        Node* R = l->right;
+        l->parrent = t->parrent;
+        l->right = t; if (t) t->parrent = l;
+        t->left = R; if (R) R->parrent = t;
+        t->h = std::max(Height(t->left), Height(t->right)) + 1;
+        l->h = std::max(Height(l->left), Height(l->right)) + 1;
 
         return l;
     }
 
-    size_t Height(Node* t)
-    {
+    size_t Height(Node* t) {
         if (!t) return 0;
         return t->h;
     }
-    
-    Node* insert(Node* t, T elem)
-    {
-        if (!t)
-        {
-            return new Node(elem);;
+
+    Node* insert(Node* t, T elem) {
+        if (!t) {
+            return new Node(elem);
         }
 
-        if (t->data < elem)
-        {
-            t->r = insert(t->r, elem);
-            if (t->r)
-                t->r->p = t;
-        }
-        else if (elem < t->data)
-        {
-            t->l = insert(t->l, elem);
-            if (t->l)
-                t->l->p = t;
-        }
-        else
+        if (t->data < elem) {
+            t->right = insert(t->right, elem);
+            if (t->right) {
+                t->right->parrent = t;
+            }
+        } else if (elem < t->data) {
+            t->left = insert(t->left, elem);
+            if (t->left) {
+                t->left->parrent = t;
+            }
+        } else {
             return t;
+        }
 
-        t->h = std::max(Height(t->l), Height(t->r)) + 1;
+        t->h = std::max(Height(t->left), Height(t->right)) + 1;
 
         t = Balance(t);
 
         return t;
     }
 
-    Node* erase(Node* t, T elem)
-    {
-        if (!t)
-        {
+    Node* erase(Node* t, T elem) {
+        if (!t) {
             return nullptr;
         }
 
-        if (t->data < elem)
-        {
-            t->r = erase(t->r, elem);
-            if (t->r)
-                t->r->p = t;
-        }
-        else if (elem < t->data)
-        {
-            t->l = erase(t->l, elem);
-            if (t->l)
-                t->l->p = t;
-        }
-        else
-        {
-            /*if (!t->l)
-            {
-                Node* h = t->r;
-                t->r = nullptr;
-                if (h)
-                    h->p = t->p;
+        if (t->data < elem) {
+            t->right = erase(t->right, elem);
+            if (t->right) {
+                t->right->parrent = t;
+            }
+        } else if (elem < t->data) {
+            t->left = erase(t->left, elem);
+            if (t->left) {
+                t->left->parrent = t;
+            }
+        } else {
+            if (!t->right) {
+                Node* h = t->left;
+                t->left = nullptr;
+                if (h) {
+                    h->parrent = t->parrent;
+                }
                 delete t;
                 return h;
-            }
-            else */
-            if (!t->r)
-            {
-                Node* h = t->l;
-                t->l = nullptr;
-                if (h)
-                    h->p = t->p;
-                delete t;
-                return h;
-            }
-            else {
-                Node* h = t->r;
-                while (h->l)
-                    h = h->l;
+            } else {
+                Node* h = t->right;
+                while (h->left)
+                    h = h->left;
                 t->data = h->data;
-                t->r = erase(t->r, h->data);
-                if (t->r)
-                    t->r->p = t;
+                t->right = erase(t->right, h->data);
+                if (t->right)
+                    t->right->parrent = t;
             }
         }
 
-        t->h = std::max(Height(t->l), Height(t->r)) + 1;
+        t->h = std::max(Height(t->left), Height(t->right)) + 1;
 
         t = Balance(t);
 
         return t;
     }
 
-    int Dh(Node* t) {
-        if (!t)
+    int HeightDifference(Node* t) {
+        if (!t) {
             return 0;
-        return Height(t->l) - Height(t->r);
+        }
+        return Height(t->left) - Height(t->right);
     }
 
-    Node* Balance(Node* t)
-    {
-        int dh = Dh(t);
+    Node* Balance(Node* t) {
+        int dh = HeightDifference(t);
 
-        if (dh > 1 && Dh(t->l) >= 0) {
+        if (dh > 1 && HeightDifference(t->left) >= 0) {
             return RRotate(t);
         }
-        if (dh > 1 && Dh(t->l) < 0) {
-            t->l = LRotate(t->l);
+        if (dh > 1 && HeightDifference(t->left) < 0) {
+            t->left = LRotate(t->left);
             return RRotate(t);
         }
 
-        if (dh < -1 && Dh(t->r) <= 0) {
+        if (dh < -1 && HeightDifference(t->right) <= 0) {
             return LRotate(t);
         }
-        if (dh < -1 && Dh(t->r) > 0) {
-            t->r = RRotate(t->r);
+        if (dh < -1 && HeightDifference(t->right) > 0) {
+            t->right = RRotate(t->right);
             return LRotate(t);
         }
 
